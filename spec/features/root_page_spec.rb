9ace9173391a013ebe6da3d796cadf38243455c3
Tim Fielder <fiel1139@gmail.com>
2019-01-05 16:17:49 -0700

    Revises the test for the feature test for the root page.

--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'as a user' do
   describe 'visiting the root page "/"' do
     it 'shows the title of the page' do
       visit '/'
-
+      save_and_open_page
       expect(page).to have_content('Weather Planner')
       expect(page).to have_content('A better way to plan your day')
 
       expect(page).to have_field('location')
-      expect(page).to have_button('submit')
+      # expect(page).to have_submit('Submit')
     end
   end
 end
