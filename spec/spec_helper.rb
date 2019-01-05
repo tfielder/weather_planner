d963dc5c0e6fa4edc42140c9a042e68676e872c1
Tim Fielder <fiel1139@gmail.com>
2019-01-05 15:41:04 -0700

    Organizes Database cleaner code in spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,7 +36,7 @@ RSpec.configure do |config|
       DatabaseCleaner.clean_with(:truncation)
     end
     config.around(:each) do |example|
-        DatabaseCleaner.cleaning do
+      DatabaseCleaner.cleaning do
         example.run
       end
     end
