7857cd609d96f14ce127e2fc42aacfe3ea90d765
Tim Fielder <fiel1139@gmail.com>
2019-01-05 16:18:44 -0700

    Removes the save_and_open_page in the root page spec.

--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'as a user' do
   describe 'visiting the root page "/"' do
     it 'shows the title of the page' do
       visit '/'
-      save_and_open_page
+
       expect(page).to have_content('Weather Planner')
       expect(page).to have_content('A better way to plan your day')
 
