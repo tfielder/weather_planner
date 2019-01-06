445fd99dec5b2f7b31c2ca266f960cdb56d0376f
Tim Fielder <fiel1139@gmail.com>
2019-01-06 12:00:00 -0700

    Adds Today tab information to the results page spec.

--- spec/features/results_page_spec.rb
+++ spec/features/results_page_spec.rb
@@ -18,6 +18,33 @@ describe 'as a user' do
 
         expect(page).to have_button("Home")
       end
+      it 'displays todays conditions' do
+        visit '/'
+
+        fill_in 'Location', with: 'Denver, CO'
+
+        click_on 'Submit'
+
+        expect(current_path).to eq('/results')
+
+        expect(page).to have_content("Today")
+
+        expect(page).to have_css("clothing-image")
+        expect(page).to have_css("city")
+        expect(page).to have_css("weather-summary")
+        expect(page).to have_css("current-weather-icon")
+        expect(page).to have_css("current-temp")
+        expect(page).to have_css("day-of-the-week")
+        expect(page).to have_css("todays-high")
+        expect(page).to have_css("todays-low")
+        expect(page).to have_css("now")
+        expect(page).to have_css("in-one-hour")
+        expect(page).to have_css("in-two-hours")
+        expect(page).to have_css("in-three-hours")
+        expect(page).to have_css("in-four-hours")
+        expect(page).to have_css("in-five-hours")
+        expect(page).to have_css("in-six-hours")
+      end
     end
   end
 end
