991291849a396a5bf00d9d03cde8b7313440335a
Tim Fielder <fiel1139@gmail.com>
2019-01-05 17:49:17 -0700

    Adds the template for the results page spec.

---
+++ spec/features/results_page_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'as a user' do
+  describe 'after successfully submitting a location on "/"' do
+    describe 'directs the user to the "/results" page' do
+      it 'shows a selection box for "Today"' do
+      end
+      it 'shows a selection box for "Next 5 days"' do
+      end
+      it 'shows a selection box for "Next 10 days"' do
+      end
+    end
+  end
+end
