01fee1c6a7cfec2acbffa29e558e1f31d5887b8f
Tim Fielder <fiel1139@gmail.com>
2019-01-05 17:51:52 -0700

    Creates test for results page.

--- spec/features/results_page_spec.rb
+++ spec/features/results_page_spec.rb
@@ -3,11 +3,17 @@ require 'rails_helper'
 describe 'as a user' do
   describe 'after successfully submitting a location on "/"' do
     describe 'directs the user to the "/results" page' do
-      it 'shows a selection box for "Today"' do
-      end
-      it 'shows a selection box for "Next 5 days"' do
-      end
-      it 'shows a selection box for "Next 10 days"' do
+      it 'shows a selection boxes' do
+        visit '/'
+
+        fill_in :location, with: 'Denver, CO'
+
+        click_on 'Submit'
+
+        expect(current_path).to eq('/results')
+        expect(page).to have_link("Today")
+        expect(page).to have_link("Next 5 Days")
+        expect(page).to have_link("Next 10 Days")
       end
     end
   end
