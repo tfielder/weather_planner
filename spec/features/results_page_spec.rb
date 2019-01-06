f6e43703fbd4b38109b2abfadff1fc6e69a3288a
Tim Fielder <fiel1139@gmail.com>
2019-01-06 12:37:44 -0700

    Rearranges results page spec.

--- spec/features/results_page_spec.rb
+++ spec/features/results_page_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'as a user' do
   describe 'after successfully submitting a location on "/"' do
-    describe 'directs the user to the "/results" page' do
-      it 'shows a selection boxes' do
+    describe 'displays the "/results" page' do
+      it 'shows the results page' do
         visit '/'
 
         fill_in 'Location', with: 'Denver, CO'
@@ -17,17 +17,8 @@ describe 'as a user' do
         expect(page).to have_content("Next 10 Days")
 
         expect(page).to have_button("Home")
-      end
-      it 'displays todays conditions' do
-        visit '/'
 
-        fill_in 'Location', with: 'Denver, CO'
-
-        click_on 'Submit'
-
-        expect(current_path).to eq('/results')
-
-        expect(page).to have_content("Today")
+        #Today section
         expect(page).to have_content("You should wear:")
 
         expect(page.find(".clothing-image")[:class]).to eq("clothing-image")
@@ -46,6 +37,10 @@ describe 'as a user' do
         expect(page.find(".in-four-hours")[:class]).to eq("in-four-hours")
         expect(page.find(".in-five-hours")[:class]).to eq("in-five-hours")
         expect(page.find(".in-six-hours")[:class]).to eq("in-six-hours")
+
+        #Next 5 days section
+
+        #Next 10 days section
       end
     end
   end
