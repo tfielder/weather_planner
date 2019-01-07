2a41dcd651cd3a89a7185c1fdc57e3f94e32b040
Tim Fielder <fiel1139@gmail.com>
2019-01-06 22:29:38 -0700

    Adds facade to home controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,6 @@ class HomeController < ApplicationController
   end
 
   def show
-    binding.pry
     unformatted_results = ResultsService.new(params[:location]).render_results
     @results = ResultsFacade.new(unformatted_results)
   end
