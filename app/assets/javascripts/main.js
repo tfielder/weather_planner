caa0120f377203658db1976772e2444350324622
Tim Fielder <fiel1139@gmail.com>
2019-01-07 15:57:26 -0700

    Create day divs dynamically with js.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,28 +9,47 @@ $("button").each(function () {
 // $('.clothing_bottom').empty();
 // $('.clothing_accessories').empty();
 
-const setWeather = () => {
-  if ( $(".current-weather-icon").is('clear-day')) {
-    $('.current-weather-icon').append(`<%= image_tag "weather_assets/clear-day.png" %>`);
-  } else if ($(".current-weather-icon").is('clear-night')) {
-    $('.current-weather-icon').append(`<%= image_tag "weather_assets/clear-night.png" %>`);
-  } else if ($(".current-weather-icon").is('rain')) {
-    $('.current-weather-icon').append(`<%= image_tag "weather_assets/rain.png" %>`);
-  } else if ($(".current-weather-icon").is('snow')) {
-    $('.current-weather-icon').append(`<%= image_tag "weather_assets/snow.png" %>`);
-  } else if ($(".current-weather-icon").is('sleet')) {
-    $('.current-weather-icon').append(`<%= image_tag "weather_assets/sleet.png" %>`);
-  } else if ($(".current-weather-icon").is('wind')) {
-    $('.current-weather-icon').append(`<%= image_tag "weather_assets/wind.png" %>`);
-  } else if ($(".current-weather-icon").is('fog')) {
-    $('.current-weather-icon').append(`<%= image_tag "weather_assets/fog.png" %>`);
-  } else if ($(".current-weather-icon").is('cloudy')) {
-    $('.current-weather-icon').append(`<%= image_tag "weather_assets/cloudy.png" %>`);
-  } else if ($(".current-weather-icon").is('partly-cloudy-day')) {
-    $('.current-weather-icon').append(`<%= image_tag "weather_assets/partly-cloudy-day.png" %>`);
-  } else if ($(".current-weather-icon").is('partly-cloudy-night')) {
-    $('.current-weather-icon').append(`<%= image_tag "weather_assets/partly-cloudy-night.png" %>`);
+const setWeeklyForecast = () => {
+  var i;
+  for (i = 1; i < 8; i++) {
+    //create div class
+    $("weekly-forecast-main").append(`<div class="day-${i}"></div>`);
+    $(`"day-${i}"`).append(`<div class="day-contents" class="day-of-the-week-${i}"><%= "#{@results.day_of_the_week(${i})}" %></div>`);
+    $(`"day-${i}"`).append(`<div class="day-contents" class="days-icon-${i}"><%= "#{@results.icon_of_the_day(${i})}" %></div>`);
+    $(`"day-${i}"`).append(`<div class="day-contents" class="days-high-${i}"><%= "#{@results.high_of_the_day(${i})}" %>&deg;</div>`);
+    $(`"day-${i}"`).append(`<div class="day-contents" class="days-low-${i}"><%= "#{@results.low_of_the_day(${i})}" %>&deg;</div>`);
+  };
+}
+
+const setWeather = (klass) => {
+  if ( $(klass).is('clear-day')) {
+    $(klass).append(`<%= image_tag "weather_assets/clear-day.png" %>`);
+  } else if ($(klass).is('clear-night')) {
+    $(klass).append(`<%= image_tag "weather_assets/clear-night.png" %>`);
+  } else if ($(klass).is('rain')) {
+    $(klass).append(`<%= image_tag "weather_assets/rain.png" %>`);
+  } else if ($(klass).is('snow')) {
+    $(klass).append(`<%= image_tag "weather_assets/snow.png" %>`);
+  } else if ($(klass).is('sleet')) {
+    $(klass).append(`<%= image_tag "weather_assets/sleet.png" %>`);
+  } else if ($(klass).is('wind')) {
+    $(klass).append(`<%= image_tag "weather_assets/wind.png" %>`);
+  } else if ($(klass).is('fog')) {
+    $(klass).append(`<%= image_tag "weather_assets/fog.png" %>`);
+  } else if ($(klass).is('cloudy')) {
+    $(klass).append(`<%= image_tag "weather_assets/cloudy.png" %>`);
+  } else if ($(klass).is('partly-cloudy-day')) {
+    $(klass).append(`<%= image_tag "weather_assets/partly-cloudy-day.png" %>`);
+  } else if ($(klass).is('partly-cloudy-night')) {
+    $(klass).append(`<%= image_tag "weather_assets/partly-cloudy-night.png" %>`);
   } else {
-    $('.current-weather-icon').append(`<p> "No weather today" </p>`);
+    $(klass).append(`<p> "No weather today" </p>`);
   }
 }
+
+const setWeatherIcons = () => {
+  setCurrentWeather(".current-weather-icon");
+}
+
+setWeather(".current-weather-icon");
+
